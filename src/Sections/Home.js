f6325163a54cdaf61f17f3a20182bdcbeba55b18
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-05 15:44:22 +0200

    created a logo on navbar

--- src/Sections/Home.js
+++ src/Sections/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
-// import Navbar from '../components/Navbar/Navbar'
+import Navbar from '../components/Navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
-      {/* <Navbar/> */}
+      <Navbar/>
     </div>
   )
 }
