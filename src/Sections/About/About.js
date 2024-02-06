87a12de455ab7663c7958e48382dea0bd060e2ca
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-06 13:50:08 +0200

    Added information button  to include my skills on About component

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -12,12 +12,35 @@ const About = () => {
       {/* About Content */}
       <div className="about-container">
         <div className="about-img">
-        <img src="/Images/Vuyo_img.png" alt="" width="40px" height="40px" />
+        <img src="/Images/Vuyo_img.png" alt="" />
         </div>
         <div className="about-text">
           <p>
-          I studied Software development at the Walter Sisulu University in the Eastern Cape.I am a keen, hard working, reliable and excellent time keeper,  bright and receptive person, able to communicate well with people at all levels. I am good at working using my own initiative and I am flexible in my approach to work duties. I have a good sense of humour and a pleasant approach.
+          I studied Software development at the Walter Sisulu University in the Eastern Cape.I am a keen, hard working, reliable and excellent time keeper,  bright and receptive person, able to communicate well with people at all levels. I am good at working using my own initiative 
+          and I am flexible in my approach to work duties. I have a good sense of humour and a pleasant approach.
           </p>
+
+          <h2>My skills</h2>
+          {/* Box 2 */}
+          <div className="skills-container">
+            <div className="skill">
+                <button className="btn">HTML</button>
+                <button className="btn">CSS</button>
+                <button className="btn">JavaScript</button>
+            </div>
+
+            <div className="skill">
+                <button className="btn">NodeJs</button>
+                <button className="btn">React</button>
+                <button className="btn">Bootstrap</button>
+            </div>
+
+            <div className="skill">
+                <button className="btn">VB.Net</button>
+                <button className="btn">Java</button>
+                <button className="btn">Python</button>
+            </div>
+            </div>
         </div>
       </div>
     </section>
