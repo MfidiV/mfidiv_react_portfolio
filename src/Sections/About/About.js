4463b3100586ac61d50106aaa4ae94acc57186da
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-06 14:13:55 +0200

    Hovering effect on my skills information buttons

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './about.css'; // Make sure to import the CSS file
 
 const About = () => {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <section className="about" id="about">
       <div className="heading">
@@ -12,35 +14,52 @@ const About = () => {
       {/* About Content */}
       <div className="about-container">
         <div className="about-img">
-        <img src="/Images/Vuyo_img.png" alt="" />
+          <img src="/Images/Vuyo_img.png" alt="" />
         </div>
         <div className="about-text">
           <p>
-          I studied Software development at the Walter Sisulu University in the Eastern Cape.I am a keen, hard working, reliable and excellent time keeper,  bright and receptive person, able to communicate well with people at all levels. I am good at working using my own initiative 
-          and I am flexible in my approach to work duties. I have a good sense of humour and a pleasant approach.
+            I studied Software development at the Walter Sisulu University in the Eastern Cape. I am a keen, hardworking, reliable, and excellent timekeeper, bright and receptive person, able to communicate well with people at all levels. I am good at working using my own initiative and I am flexible in my approach to work duties. I have a good sense of humor and a pleasant approach.
           </p>
 
           <h2>My skills</h2>
           {/* Box 2 */}
           <div className="skills-container">
             <div className="skill">
-                <button className="btn">HTML</button>
-                <button className="btn">CSS</button>
-                <button className="btn">JavaScript</button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'HTML'}
+              </button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'CSS'}
+              </button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'JavaScript'}
+              </button>
             </div>
 
             <div className="skill">
-                <button className="btn">NodeJs</button>
-                <button className="btn">React</button>
-                <button className="btn">Bootstrap</button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'NodeJs'}
+              </button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'React'}
+              </button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'Bootstrap'}
+              </button>
             </div>
 
             <div className="skill">
-                <button className="btn">VB.Net</button>
-                <button className="btn">Java</button>
-                <button className="btn">Python</button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'VB.Net'}
+              </button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'Java'}
+              </button>
+              <button className="btn" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                {hovered ? 'Seal' : 'Python'}
+              </button>
             </div>
-            </div>
+          </div>
         </div>
       </div>
     </section>
