9f22782dfc32a04a3f5968c7b22652476ec2a559
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-09 14:08:00 +0200

    Inserted cards to display my services and set hover effect with custom styling

--- src/Sections/Services/Services.js
+++ src/Sections/Services/Services.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import './Services.css'; // Assuming you have a CSS file named Services.css for the styles
-
+import React from 'react';
+import './Services.css'; 
 
 const Services = () => {
-  return (
-    <section className="services" id="services">
+   
+
+    return (
+        <section className="services" id="services">
             <div className="heading">
                 <h2>Services</h2>
                 <span>I offer</span>
             </div>
+            <div className="services-content">
+                {/* Box 1 */}
+                <div className="services-box">
+                    <i className='bx bx-code-alt'></i>
+                    <h3>Web Development</h3>
+                    learn more
+                </div>
+                {/* Box 2 */}
+                <div className="services-box">
+                    <i className='bx bx-server'></i>
+                    <h3>Backend Development</h3>
+                    learn more
+                </div>
+                {/* Box 3 */}
+                <div className="services-box">
+                    <i className='bx bx-brush'></i>
+                    <h3>UI/UX Design</h3>
+                    learn more
+                </div>
+            </div>
+          
         </section>
-  )
-}
+    );
+};
 
-export default Services
+export default Services;
