c6f2115f33ed0dbdaff3b226098f5c10be97b841
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-09 13:50:11 +0200

    Created the new component for services an heading inheriting global styles for class heading

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./Sections/Home/Home";
 import About from "./Sections/About/About";
+import Services from "./Sections/Services/Services";
 
 function App() {
   const currentTheme = localStorage.getItem("current_theme");
@@ -16,6 +17,7 @@ function App() {
       <Navbar theme={theme} setTheme={setTheme} />
       <Home theme={theme} />
       <About theme={theme}/>
+      <Services/>
     </div>
   );
 }
---
+++ src/Sections/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Services.css'; // Assuming you have a CSS file named Services.css for the styles
+
+
+const Services = () => {
+  return (
+    <section className="services" id="services">
+            <div className="heading">
+                <h2>Services</h2>
+                <span>I offer</span>
+            </div>
+        </section>
+  )
+}
+
+export default Services
