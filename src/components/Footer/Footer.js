f51188d783f45eb6b344ae5bbcde7739551033f0
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-12 16:12:26 +0200

    Footer component created and integrated to development

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      {/* Copyright */}
+      <div className="copyright">
+        <p>&copy; 2023 Created By <span>Vuyolwethu</span> | All Right Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
