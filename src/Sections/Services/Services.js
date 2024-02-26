6aeb037b48e56f3d214505eb9b41f13245651aa7
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-26 09:35:23 +0200

    Add comments to functions and improve readability in Services component

--- src/Sections/Services/Services.js
+++ src/Sections/Services/Services.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import ParallaxTilt from 'react-parallax-tilt';
-import './Services.css'; // Assuming you have a CSS file named Services.css for the styles
+import './Services.css'; 
 import 'boxicons/css/boxicons.min.css';
 import AOS from 'aos';
-import 'aos/dist/aos.css'
-
+import 'aos/dist/aos.css';
 
 const Services = () => {
+    // State variables for managing modal content and its visibility
     const [modalContent, setModalContent] = useState('');
     const [showModal, setShowModal] = useState(false);
 
-    useEffect(()=>{
-        AOS.init({duration:1000})
-    },[])
+    // Initializinggit add  AOS library on component mount
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    }, []);
+
+    // Function to set modal content based on the selected service
     const servicesRendered = (service) => {
         if (service === "Backend") {
             setModalContent("Experienced back-end developer with proficiency in Python, Java, and Node.js, skilled in database management, scalable applications, server technologies, RESTful APIs, and cloud platforms. Skilled in troubleshooting, debugging, and security measures, committed to efficient, reliable solutions.");
@@ -21,9 +24,10 @@ const Services = () => {
         } else {
             setModalContent("The designer is a detail-oriented UI/UX professional, specializing transforming complex concepts into intuitive, visually appealing interfaces. They are committed to staying updated with industry trends and technologies, ensuring their work remains at the forefront of modern design practice");
         }
-        setShowModal(true);
+        setShowModal(true); // Show modal after setting content
     };
 
+    // Function to close the modal
     const closeModal = () => {
         setShowModal(false);
     };
@@ -34,26 +38,27 @@ const Services = () => {
                 <h2>Services</h2>
                 <span>I offer</span>
             </div>
-            <div className="services-content" data-aos="zoom-in"  >
-                {/* Box 1 */}
+            <div className="services-content" data-aos="zoom-in">
+                {/* Service Box 1 */}
                 <ParallaxTilt className="services-box" perspective={1000} tiltMaxAngleX={15} tiltMaxAngleY={15} scale={1.1} transitionSpeed={800}>
                     <i className='bx bx-code-alt'></i>
                     <h3>Web Development</h3>
                     <span className="learn-more-link" onClick={() => servicesRendered('Frontend')}>Learn more</span>
                 </ParallaxTilt>
-                {/* Box 2 */}
+                {/* Service Box 2 */}
                 <ParallaxTilt className="services-box" perspective={1000} tiltMaxAngleX={15} tiltMaxAngleY={15} scale={1.1} transitionSpeed={800}>
                     <i className='bx bx-server'></i>
                     <h3>Backend Development</h3>
                     <span className="learn-more-link" onClick={() => servicesRendered('Backend')}>Learn more</span>
                 </ParallaxTilt>
-                {/* Box 3 */}
-                <ParallaxTilt className="services-box" perspectkljjive={1000} tiltMaxAngleX={15} tiltMaxAngleY={15} scale={1.1} transitionSpeed={800}>
+                {/* Service Box 3 */}
+                <ParallaxTilt className="services-box" perspective={1000} tiltMaxAngleX={15} tiltMaxAngleY={15} scale={1.1} transitionSpeed={800}>
                     <i className='bx bx-brush'></i>
                     <h3>UI/UX Design</h3>
                     <span className="learn-more-link" onClick={() => servicesRendered('UI/UX')}>Learn more</span>
                 </ParallaxTilt>
             </div>
+            {/* Modal for displaying service details */}
             {showModal && (
                 <div className="modal">
                     <div className="modal-content">
