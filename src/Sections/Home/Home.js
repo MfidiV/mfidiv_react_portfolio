9e88f5bc246333632f91ac3902b2737016efe385
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-26 09:14:00 +0200

    clean and comment functions on Home component

--- src/Sections/Home/Home.js
+++ src/Sections/Home/Home.js
@@ -1,29 +1,40 @@
-import React,{useEffect} from 'react';
-import './Home.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from 'react';
+import './Home.css'; // Importing the CSS file for styling
+import AOS from 'aos'; // Importing the AOS library for animations
+import 'aos/dist/aos.css'; // Importing the CSS file for AOS animations
 
 const Home = ({ theme }) => {
+
+  // Function to open PDF in a new tab
   const openPDF = () => {
+
+    // Constructing the path to the PDF file
     var pdfPath = process.env.PUBLIC_URL + '/Documents/Mfidi_V.pdf';
     window.open(pdfPath, '_blank');
   };
 
+  // Function to scroll to the contact section 
   const scrollToContact = () => {
-    const contactSection = document.getElementById('contact-container');
+    const contactSection = document.getElementById('contact-container'); // Finding the contact section element by its id
+    // If the contact section element is found
     if (contactSection) {
+      // Scroll to the contact section smoothly
       contactSection.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
-  useEffect(()=>{
-    AOS.init({duration:2000})
-},[])
+  // useEffect hook to initialize AOS library when component mounts
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []); // Empty dependency array means this effect runs only once after the component mounts
 
+  // JSX rendering
   return (
     <section className={`home ${theme === 'dark' ? 'dark-mode' : ''}`}>
       <div className="hero-image"> </div>
       <div className="home-text">
+
+        {/* Animated text */}
         <span className="animated-text" data-aos="fade-right">
           <span>H</span>
           <span>e</span>
@@ -34,15 +45,19 @@ const Home = ({ theme }) => {
           <span> </span>
           <span>I'm</span>
         </span>
+        
+        {/* Main heading */}
         <h1 data-aos="zoom-right">Vuyolwethu Mfidi</h1>
+        {/* Sub heading */}
         <h2 data-aos="fade-right">Software Developer</h2>
       </div>
 
       <div className="home-buttons" data-aos="fade-up">
+        {/* Button to download CV */}
         <button className="btn1" onClick={openPDF}>
           Download CV
         </button>
-      
+        {/* Button to scroll to contact section */}
         <button className="btn1" onClick={scrollToContact}>
           Contact Me
         </button>
@@ -51,4 +66,5 @@ const Home = ({ theme }) => {
   );
 };
 
+// Exporting the Home component as default
 export default Home;
