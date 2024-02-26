01f7fc562cccf237f58da7c7acbdda73893244bc
Vuyolwethu Mfidi <mfidiv@gmail.com>
2024-02-26 09:24:33 +0200

    Add comments to functions and improve readability in About component

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -1,12 +1,11 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './About.css'; // Make sure to import the CSS file
-import AOS from 'aos';
-import 'aos/dist/aos.css'
+import './About.css'; // Import the CSS file for styling
+import AOS from 'aos'; // Import the AOS library for animations
+import 'aos/dist/aos.css'; // Import the CSS file for AOS animations
 
 const About = ({ theme }) => {
-  // Initialize separate state variables for each button's hover state
-  // const [hoveredHTML, setHoveredHTML] = useState(false);
+  // Initialize state variables for each button's hover state
   const [hoveredCSS, setHoveredCSS] = useState(false);
   const [hoveredJavaScript, setHoveredJavaScript] = useState(false);
   const [hoveredNodeJs, setHoveredNodeJs] = useState(false);
@@ -16,23 +15,27 @@ const About = ({ theme }) => {
   const [hoveredJava, setHoveredJava] = useState(false);
   const [hoveredPython, setHoveredPython] = useState(false);
 
-   // Initialize state variable for JavaScript button text
+  // Initialize state variable for toggling JavaScript button text
   const [showPercentage, setShowPercentage] = useState(true);
 
-  useEffect(()=>{
-    AOS.init({duration:1000})
-  },[])
+  // Initialize AOS animation library on component mount
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
 
+  // Toggle showPercentage state every 2 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setShowPercentage(prevShowPercentage => !prevShowPercentage);
     }, 2000);
 
+    // Clean up interval on component unmount
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <section  className={`about ${theme === 'dark' ? 'dark-mode' : ''}`}>
+    <section className={`about ${theme === 'dark' ? 'dark-mode' : ''}`}>
+      {/* Heading */}
       <div className="heading">
         <h2>About Me</h2>
         <span>Introduction</span>
@@ -40,11 +43,14 @@ const About = ({ theme }) => {
 
       {/* About Content */}
       <div className="about-container">
+        {/* Image */}
         <div className="about-img" data-aos="fade-right">
           <img src="/Images/about-1.jpg" alt="" />
         </div>
+        
+        {/* Text */}
         <div className="about-text">
-          <p data-aos="fade-up">            
+          <p data-aos="fade-up">
             Highly motivated Software Developer with expertise in design, installation, testing, and maintenance of software systems.
             Graduated from Walter Sisulu University in the Eastern Cape with a focus on Software Development.
             Keen, hardworking, reliable, and excellent timekeeper.
@@ -53,19 +59,21 @@ const About = ({ theme }) => {
             Possesses a good sense of humor and a pleasant demeanor, enhancing team dynamics and collaboration.
           </p>
 
+          {/* Skills */}
           <h2>My skills</h2>
-          {/* Box 2 */}
           <div className="skills-container" data-aos="fade-up">
+            {/* Skill buttons */}
             <div className="skill">
-              {/* Handle hover state individually for each button */}
+              {/* HTML button */}
               <button
-                  className={`btn ${showPercentage ? 'green-bg' : 'blue-bg'}`} // Apply background color based on showPercentage
-                  onMouseEnter={() => setShowPercentage(true)}
-                  onMouseLeave={() => setShowPercentage(false)}
-                >
-                  {showPercentage ? '85%' : 'HTML'}
-          </button>
+                className={`btn ${showPercentage ? 'green-bg' : 'blue-bg'}`} //background color based on showPercentage
+                onMouseEnter={() => setShowPercentage(true)} // Show percentage when mouse enters
+                onMouseLeave={() => setShowPercentage(false)} // Hide percentage when mouse leaves
+              >
+                {showPercentage ? '85%' : 'HTML'}
+              </button>
 
+              {/* CSS button */}
               <button 
                 className="btn" 
                 onMouseEnter={() => setHoveredCSS(true)} 
@@ -73,6 +81,7 @@ const About = ({ theme }) => {
               >
                 {hoveredCSS ? '85%' : 'CSS'}
               </button>
+              
               <button 
                 className="btn" 
                 onMouseEnter={() => setHoveredJavaScript(true)} 
@@ -82,6 +91,7 @@ const About = ({ theme }) => {
               </button>
             </div>
 
+            
             <div className="skill">
               <button 
                 className="btn" 
@@ -106,6 +116,7 @@ const About = ({ theme }) => {
               </button>
             </div>
 
+         
             <div className="skill">
               <button 
                 className="btn" 
