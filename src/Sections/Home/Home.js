12442181f909af6bcdd7c859c9233844562e5a2d
Vuyolwethu Mfidi <mfidiv@gmail.com>
2025-04-06 15:45:25 +0200

    remove image and style text

--- src/Sections/Home/Home.js
+++ src/Sections/Home/Home.js
@@ -4,8 +4,11 @@ import AOS from 'aos'; // Importing the AOS library for animations
 import 'aos/dist/aos.css'; // Importing the CSS file for AOS animations
 
 const Home = ({ theme }) => {
+
   // Function to open PDF in a new tab
   const openPDF = () => {
+
+    // Constructing the path to the PDF file
     var pdfPath = process.env.PUBLIC_URL + '/Documents/Mfidi V.pdf';
     window.open(pdfPath, '_blank');
   };
@@ -13,7 +16,9 @@ const Home = ({ theme }) => {
   // Function to scroll to the contact section 
   const scrollToContact = () => {
     const contactSection = document.getElementById('contact-container'); // Finding the contact section element by its id
+    // If the contact section element is found
     if (contactSection) {
+      // Scroll to the contact section smoothly
       contactSection.scrollIntoView({ behavior: 'smooth' });
     }
   };
@@ -26,45 +31,40 @@ const Home = ({ theme }) => {
   // JSX rendering
   return (
     <section className={`home ${theme === 'dark' ? 'dark-mode' : ''}`}>
-      <div className="hero-image"></div>
+      <div className="hero-image"> </div>
+      <div className="home-text">
 
-      <div className="about-img" data-aos="fade-right">
-        <img src="/Images/about-1.jpg" alt="About Vuyolwethu Mfidi" />
+        {/* Animated text */}
+        <span className="animated-text" data-aos="fade-right">
+          <span>H</span>
+          <span>e</span>
+          <span>l</span>
+          <span>l</span>
+          <span>o</span>
+          <span>,</span>
+          <span> </span>
+          <span>I'm</span>
+        </span>
+        
+        {/* Main heading */}
+        <h1 data-aos="zoom-right">Vuyolwethu Mfidi</h1>
+        {/* Sub heading */}
+        <h2 data-aos="fade-right">Software Developer</h2>
       </div>
 
-      <div className="TextHome">
-        <div className="home-text">
-          {/* Animated text */}
-          <span className="animated-text" data-aos="fade-right">
-            <span>H</span>
-            <span>e</span>
-            <span>l</span>
-            <span>l</span>
-            <span>o</span>
-            <span>,</span>
-            <span> </span>
-            <span>I'm</span>
-          </span>
-
-          {/* Main heading */}
-          <h1 data-aos="zoom-right">Vuyolwethu Mfidi</h1>
-          {/* Sub heading */}
-          <h2 data-aos="fade-right">Software Developer</h2>
-        </div>
-
-        <div className="home-buttons" data-aos="fade-left">
-          {/* Button to download CV */}
-          <button className="btn1" onClick={openPDF}>
-            Download CV
-          </button>
-          {/* Button to scroll to contact section */}
-          <button className="btn1" onClick={scrollToContact}>
-            Contact Me
-          </button>
-        </div>
+      <div className="home-buttons" data-aos="fade-up">
+        {/* Button to download CV */}
+        <button className="btn1" onClick={openPDF}>
+          Download CV
+        </button>
+        {/* Button to scroll to contact section */}
+        <button className="btn1" onClick={scrollToContact}>
+          Contact Me
+        </button>
       </div>
     </section>
   );
 };
 
+// Exporting the Home component as default
 export default Home;
